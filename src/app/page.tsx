0f6c9525e6eac43eddff439aa367c3ffb08e76e0
Léo <leo.cigale@gmail.com>
2025-07-23 22:27:23 +0200

    fix: pt-50 to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import CardContent from "@/components/CardContent";
 export default function Home() {
   return (
     <div className="overflow-x-hidden text-white max-h-screen">
-      <main className="max-w-6xl mx-auto px-5 pt-24 overflow-y-auto">
+      <main className="max-w-6xl mx-auto px-5 pt-50 overflow-y-auto">
         <div className="flex flex-col justify-center md:flex-row md:justify-start gap-12 items-start">
           <Side />
           <div className="flex flex-col gap-8 flex-1">
